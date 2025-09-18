47100606e7969d3fb169619be8bf00a533aaefb7
BenjaminC188 <benjaminc188@nycstudents.net>
2025-09-18 05:57:53 -0700

    s

---
+++ test.py
@@ -0,0 +1,21 @@
+x = 3
+y = float(3)
+print(x,y)
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+
+"test"
+["t","e","s","t"]
+
+x = "this is a thing"
+y = x.split( )
+z = y[0]
+print(y)
+print(z)
+
